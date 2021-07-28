847ae3b1aea4bb1a4598ae5780dde94c5c501da4
Rhys Holloway <rhyswilliamholloway@gmail.com>
2021-07-28 15:40:39 -0700

    fix context creation

--- src/context.rs
+++ src/context.rs
@@ -1,20 +1,18 @@
 use engine::{
     graphics::byte_texture,
-    tetra::{graphics::Texture, Result, Context},
+    tetra::{graphics::Texture, Result},
+    EngineContext,
 };
 
 use crate::{
     battle_move::BattleMovedex,
+    id::Dex,
     item::Itemdex,
     moves::Movedex,
     pokemon::Pokedex,
     serialize::SerializedDex,
     texture::{ItemTextures, PokemonTextures, TrainerTextures},
-    id::Dex, 
 };
-
-#[cfg(feature = "audio")]
-use crate::serialize::SerializedPokemon;
 
 pub struct PokedexClientContext {
     pub health_bar: Texture,
@@ -25,34 +23,37 @@ pub struct PokedexClientContext {
 }
 
 impl PokedexClientContext {
-    pub fn new(
-        ctx: &mut Context,
-        dex: SerializedDex,
-        #[cfg(feature = "audio")] callback: impl Fn(&mut SerializedPokemon),
-    ) -> Result<Self> {
-
+    pub fn new(ctx: &mut EngineContext, dex: SerializedDex) -> Result<Self> {
         let mut pokedex = Pokedex::with_capacity(dex.pokemon.len());
 
         let mut pokemon_textures = PokemonTextures::with_capacity(dex.pokemon.len());
-    
+
         for pokemon in dex.pokemon {
             pokemon_textures.insert(ctx, &pokemon)?;
-    
-            #[cfg(feature = "audio")]
-            let mut pokemon = pokemon;
-    
-            #[cfg(feature = "audio")]
-            (callback)(&mut pokemon);
-    
+
+            #[cfg(feature = "audio")] {
+                use engine::audio::{serialized::SerializedSoundData as SoundData, sound::Sound};
+
+                if !pokemon.cry_ogg.is_empty() {
+                    engine::audio::add_sound(&ctx.audio.sound, SoundData {
+                        bytes: pokemon.cry_ogg,
+                        sound: Sound {
+                            name: crate::CRY_ID,
+                            variant: Some(pokemon.pokemon.id),
+                        },
+                    })
+                }
+            }
+
             pokedex.insert(pokemon.pokemon.id, pokemon.pokemon);
         }
-    
+
         Pokedex::set(pokedex);
-    
+
         let mut movedex = Movedex::with_capacity(dex.moves.len());
-    
+
         let mut battle_movedex = BattleMovedex::with_capacity(0);
-    
+
         for serialized_move in dex.moves {
             let pmove = serialized_move.pokemon_move;
             if let Some(battle_move) = serialized_move.battle_move {
@@ -60,28 +61,28 @@ impl PokedexClientContext {
             }
             movedex.insert(pmove.id, pmove);
         }
-    
+
         Movedex::set(movedex);
         BattleMovedex::set(battle_movedex);
-    
+
         let mut itemdex = Itemdex::with_capacity(dex.items.len());
-    
+
         let mut item_textures = ItemTextures::with_capacity(dex.items.len());
-    
+
         for item in dex.items.into_iter() {
             item_textures.insert(item.item.id, Texture::from_file_data(ctx, &item.texture)?);
             itemdex.insert(item.item.id, item.item);
         }
-    
+
         let trainer_textures = TrainerTextures(
             dex.trainers
                 .into_iter()
                 .map(|(k, bytes)| (k, Texture::from_file_data(ctx, &bytes).unwrap()))
                 .collect(),
         );
-    
+
         Itemdex::set(itemdex);
-    
+
         let item_textures = ItemTextures(item_textures);
 
         Ok(Self {
